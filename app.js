1d7ddd59a6dfdd2d608d7526dacd3d8fba7f49a1
Tobias Hahnen <hahnen.tobi@gmail.com>
2017-05-23 19:35:15 +0200

    Extra für Claus
    <3

---
+++ app.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $.getJSON( "containerstandorte.json", function( data ) {
+        create_map(data)
+    });
+});
+
+function create_map(json_data) {
+	// Hier alles wichtige einfügen!
+}
